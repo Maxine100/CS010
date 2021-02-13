48af8d772457a070cf2f979d19897887f5159da5
Maxine Wu <maxine.wu00@gmail.com>
2021-02-12 16:07:41 -0800

    Edited 7.14.cpp.

--- Ch7Labs/7.14.cpp
+++ Ch7Labs/7.14.cpp
@@ -5,28 +5,22 @@
 using namespace std;
 
 int LargestNumber(int num1, int num2, int num3) {
-	int largestNumber;
-	if (num1 > num2 && num1 > num3) {
-		largestNumber = num1;
-	}
-	if (num2 > num1 && num2 > num3) {
+	int largestNumber = num1;
+	if (num2 > largestNumber) {
 		largestNumber = num2;
 	}
-	if (num3 > num1 && num3 > num2) {
+	if (num3 > largestNumber) {
 		largestNumber = num3;
 	}
 	return largestNumber;
 }
 
 int SmallestNumber(int num1, int num2, int num3) {
-	int smallestNumber;
-	if (num1 < num2 && num1 < num3) {
-		smallestNumber = num1;
-	}
-	if (num2 < num1 && num2 < num3) {
+	int smallestNumber = num1;
+	if (num2 < smallestNumber) {
 		smallestNumber = num2;
 	}
-	if (num2 < num1 && num3 < num2) {
+	if (num3 < smallestNumber) {
 		smallestNumber = num3;
 	}
 	return smallestNumber;
